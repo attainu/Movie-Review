459a16ea842ad8221c30bfadf1c8f0ff50911c63
Deepam Bahre <56350721+deepam-bahre-au6@users.noreply.github.com>
2020-04-12 23:18:10 +0530

    added find movie route

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
-/*
-mongoose.connect('mongo "mongodb+srv://cluster0-iiers.mongodb.net/test"  --username deepam_bahre', {
+
+mongoose.connect('mongodb+srv://deepam_bahre:<password>@cluster0-iiers.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
-*/
+
+/*
 mongoose.connect('mongodb://127.0.0.1:27017/movie-review-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
+*/
