3ed1204afc8ef91575893fb5c978d100c67ffdda
Deepam Bahre <56350721+deepam-bahre-au6@users.noreply.github.com>
2020-04-12 11:21:45 +0530

    update package.json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const reviewRouter = require('./routers/review')
 const adminRouter = require('./routers/admin')
-
+const morgan = require('morgan');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(userRouter)
 app.use(reviewRouter)
 app.use(adminRouter)
-
+app.use(morgan('dev'));
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
