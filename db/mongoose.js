c4392b57c1ace7f09c11977872527bf38f3a57d2
Deepam Bahre <56350721+deepam-bahre-au6@users.noreply.github.com>
2020-04-12 23:15:44 +0530

    added find movie route

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
-
+/*
 mongoose.connect('mongo "mongodb+srv://cluster0-iiers.mongodb.net/test"  --username deepam_bahre', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
-/*
+*/
 mongoose.connect('mongodb://127.0.0.1:27017/movie-review-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
-*/
--- routers/movie.js
+++ routers/movie.js
@@ -13,4 +13,25 @@ router.get('/Allmovies', async (req, res) => {
       }
   })
 
+
+  router.get('/Findmovies', async (req, res) => {
+    try {
+    const findmovie = await Movie.find({ Name: { $regex: "t", $options: "i" } }, function(err, docs) {
+      console.log("Partial Search Begins");
+      console.log(docs);
+      })
+      res.json(findmovie)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+      }
+  });
+
+
 module.exports = router
+
+
+      /*
+  Movie.find({ name: { $regex: "s", $options: "i" } }, function(err, docs) {
+    console.log("Partial Search Begins");
+    console.log(docs);
+    });*/
