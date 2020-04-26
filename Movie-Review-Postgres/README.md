f7f60f1bd6919f2ce01d90e38f19e1a2036f1526
Deepam Bahre <56350721+deepam-bahre-au6@users.noreply.github.com>
2020-04-27 01:06:42 +0530

    initial commit

--- Movie-Review-Postgres/README.md
+++
@@ -1,2 +0,0 @@
-# Movie-Review
-Project by Vibhor and Deepam
--- Movie-Review-Postgres/app/tests/movie.test.js
+++
@@ -1,8 +0,0 @@
-const server = require("../server");
-const jwt = require('jsonwebtoken')
-const request = require("supertest");
-
-
-
-
-
